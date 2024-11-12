b2db142fdf70737c1c566848aaffc7410ea9a0ce
jahnavi571 <jahnaviharish15@gmail.com>
2024-11-11 21:08:00 -0800

    Initial commit

---
+++ src/LinkedList.java
@@ -0,0 +1,14 @@
+public class LinkedList {
+    Node head;
+
+    LinkedList(){
+        head = null;
+    }
+
+    public void add(int data){
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+}
